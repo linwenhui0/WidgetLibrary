dff04bcf8fbd107e30e8f9b3be8f6e8198c8f086
linwenhui0 <1252908234@qq.com>
2020-03-07 20:48:42 +0800

    feat:修改 gradle 地址

--- app/src/main/java/com/hlibrary/widget/listview/SideBarListView.java
+++ app/src/main/java/com/hlibrary/widget/listview/SideBarListView.java
@@ -12,57 +12,60 @@ import com.hlibrary.widget.listener.DefaultIndexer;
 
 public class SideBarListView extends ListView {
 
-	private SideBar sideBar;
+    private SideBar sideBar;
 
-	public SideBarListView(Context context) {
-		super(context);
-	}
+    public SideBarListView(Context context) {
+        super(context);
+    }
 
-	public SideBarListView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public SideBarListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public SideBarListView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-	}
+    public SideBarListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-	public void setSideBar(SideBar sideBar) {
-		this.sideBar = sideBar;
-	}
+    public void setSideBar(SideBar sideBar) {
+        this.sideBar = sideBar;
+    }
 
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged(w, h, oldw, oldh);
-		if (sideBar != null)
-			sideBar.onSizeChanged(w, h, oldw, oldh);
-	}
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (sideBar != null) {
+            sideBar.onSizeChanged(w, h, oldw, oldh);
+        }
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		if (sideBar != null)
-			sideBar.draw(canvas);
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (sideBar != null) {
+            sideBar.draw(canvas);
+        }
+    }
 
-	@Override
-	public void setAdapter(ListAdapter adapter) {
-		super.setAdapter(adapter);
-		if (sideBar != null)
-			sideBar.setAdapter(adapter);
+    @Override
+    public void setAdapter(ListAdapter adapter) {
+        super.setAdapter(adapter);
+        if (sideBar != null) {
+            sideBar.setAdapter(adapter);
+        }
+    }
 
-	}
-
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev) {
-		if (sideBar != null && sideBar.onTouchEvent(ev))
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (sideBar != null && sideBar.onTouchEvent(ev)) {
 			return true;
-		return super.dispatchTouchEvent(ev);
-	}
+		}
+        return super.dispatchTouchEvent(ev);
+    }
 
-	public interface Indexerable {
+    public interface Indexerable {
 
-		DefaultIndexer getIndexer();
+        DefaultIndexer getIndexer();
 
-	}
+    }
 
 }
